311b74a5f3a771d032402cf820f8f9d45f17b3ee
Rehanpatel3858 <rhanpatel.i745@gmail.com>
2025-07-29 07:35:56 -0500

    initial command

---
+++ bot.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client= OpenAI(
+    api_key="your_api_key",
+    base_url="https://api.groq.com/openai/v1"
+)
+while True:
+    user_input= input("you:")
+    if user_input=="quit":
+        print("bot: GOOD BY")
+        break
+    response = client.chat.completions.create(
+        model= "llama3-70b-8192",
+        messages= [
+            {"role": "system","content": "you are a health bot,provide information all type of cooking and healt  related question and also provide personal opinion"},
+            {"role": "user", "content": user_input}
+        ]
+    )
+    print("bot:", response.choices[0].message.content) 
